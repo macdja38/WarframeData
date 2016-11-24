94e33dffd8613959391fc0b3e7362efdc054aa52
macdja38 <jakeincanada@icloud.com>
2016-11-24 14:14:58 -0500

    parse Mission Decks into YAML

---
+++ parsers/MissionDecks.js
@@ -0,0 +1,23 @@
+/**
+ * Created by macdja38 on 2016-11-24.
+ */
+let fs = require('fs');
+let stringify = require('json-stable-stringify');
+
+let rawText = fs.readFileSync('MissionDecks.txt', "utf8");
+
+let jsonObject = {};
+
+rawText = rawText.replace(/Stripped=True/g, "");
+rawText = rawText.replace(/(\[[0-9a-zA-Z/].*])\n((?:.|\n|\r)*?)\n?\n?\n?(?=(?:\n\[[0-9a-zA-Z/].*]|$))/g, (match, m1, m2) => {
+  jsonObject[m1.replace(/^\[(.*)]$/, "$1")] = m2;
+  m2 = m2.replace(/ - /g, "   - ");
+  m2 = m2.replace(/((?:Rotation) \w):\n((?:.|\n)*?)(?=Rotation|$)/g, (match, m1, m2) => {
+    return `  ${m1}:\n   -${m2.replace(/\n(?=..)/g, "\n   -")}\n`;
+  });
+  return `${m1}:\n  Locations:\n${m2}\n`
+});
+
+fs.writeFileSync('YAML/MissionDecks.yaml', rawText);
+
+fs.writeFileSync('JSON/MissionDecks.json', stringify(jsonObject, {space: "  "}));
