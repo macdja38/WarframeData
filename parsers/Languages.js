42df23824f4b89ab32987fe5fb3c88bec288e879
macdja38 <jakeincanada@icloud.com>
2016-11-23 19:42:44 -0500

    fix to yaml conversion of Languages.json

--- parsers/Languages.js
+++ parsers/Languages.js
@@ -3,10 +3,8 @@ let fs = require('fs');
 
 let rawText = fs.readFileSync('Languages.txt', "utf8");
 
-rawText = rawText.replace(/\[.*]/g, '$&:');
-rawText = rawText.replace(/[^\n]"(?!\n|\r\n|\r)/gm, '\\"');
-rawText = rawText.replace(/"(?:.|\n|\r\n|\r)*?\\{0}?"/g, (match)=>{
-  return `  ${match.replace(/(?:\n|\r\n|\r)/g, "\n  ")}`
+rawText = rawText.replace(/(\[[0-9a-zA-Z/].*])\n"((?:.|\n|\r)*?)"\n?\n?\n?(?=(?:\n\[[0-9a-zA-Z/].*]|$))/g, (match, m1, m2)=>{
+  return `${m1}:\n  "${m2.replace(/(?:\n|\r\n|\r)/g, "\n  ").replace(/"/g, '\\"')}"\n`
 });
 
 fs.writeFileSync('YAML/Languages.yaml', rawText);
