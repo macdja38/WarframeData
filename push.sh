44fbef1624a9767fdbed35874be49674e8e4109a
macdja38 <jakeincanada@icloud.com>
2016-11-25 19:49:50 -0500

    automate commiting build results back to github

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add /YAML/*.yaml
+  git add /JSON/*.json
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add master https://${GH_TOKEN}@github.com/macdja38/WarframeData.git > /dev/null 2>&1
+  git push
+}
+
+setup_git
+commit_website_files
+upload_files
